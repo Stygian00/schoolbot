bf9eea516c02355c339eee14288601fe83fbc1ef
Anusha <anu123dubey456@gmail.com>
2026-04-18 19:10:39 +0530

    Add functions for basic arithmetic and prime checking

---
+++ debug.py
@@ -0,0 +1,32 @@
+def f1(n):
+    "multiply n by 2"
+    print(f"Input to f1: {n}")
+    a= n * 2
+    return a
+
+def f2(n):
+    f(n)
+    "checks for prime"
+    print("f2 run")
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+def f(n):
+    "print list of i in the range of n"
+    return [i for i in range(n)]
+
+def f3(n):
+    "print function to understand the flow of the program"
+    print(f"Processing input: {n}") 
+    f1_result = f1(n)
+    print(f"Result of f1: {f1_result}")
+    f2_result = f2(n)
+    print(f"Result of f2: {f2_result}")
+    return f1_result, f2_result
+
+f1(5)
+f2(5)
+f3(5)
